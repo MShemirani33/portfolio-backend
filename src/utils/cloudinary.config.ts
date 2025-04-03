547d979db73269050bce0bbd2c4000c4ac00123f
varmanli <varmanliamirhosein@gmail.com>
2025-04-03 13:08:19 +0330

    debug deploy image

--- src/utils/cloudinary.config.ts
+++ src/utils/cloudinary.config.ts
@@ -1,9 +1,10 @@
 import { v2 as cloudinary } from 'cloudinary';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); // لود کردن env
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  url: process.env.CLOUDINARY_URL, // استفاده از URL برای تنظیمات
 });
 
 export { cloudinary };
