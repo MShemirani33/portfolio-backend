2a8ebdbeb35df85a76b0b40f9d19115fb7051d3a
varmanli <varmanliamirhosein@gmail.com>
2025-04-04 11:15:47 +0330

    same site confige

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -27,7 +27,7 @@ export class AuthController {
     res.cookie('admin_token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
       path: '/',
     });
