933c5282f9f0af36584edb153fdd36c692aaeb7f
MShemirani33 <m.shemirani33@gmail.com>
2025-04-12 23:03:47 +0330

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,15 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // const config = new DocumentBuilder()
-  //   .setTitle('API Documentation')
-  //   .setDescription('مستندات REST API پروژه')
-  //   .setVersion('1.0')
-  //   .addBearerAuth() // اگر نیاز به احراز هویت داری
-  //   .build();
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('مستندات REST API پروژه')
+    .setVersion('1.0')
+    .addBearerAuth() // اگر نیاز به احراز هویت داری
+    .build();
 
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api-docs', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   app.use(cookieParser());
   app.enableCors({
